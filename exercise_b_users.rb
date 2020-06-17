8c49929d240ac9270b6ccb953c6ebf71f0c97019
Stephen <stephen_h369@live.co.uk>
2020-06-17 18:20:34 +0100

    #6 exercise b complete

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -72,9 +72,12 @@ users = {
 
 # 5. Get the smallest of Erik's lottery numbers
 
-p users["Erik"][:lottery_numbers].min()
+# p users["Erik"][:lottery_numbers].min()
 
 # 6. Return an array of Avril's lottery numbers that are even
+
+p users["Avril"][:lottery_numbers].select(&:even?)
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "Fluffy"
