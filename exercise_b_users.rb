d730bcd2a2337d482e412c114ff218a345bb4aad
Stephen <stephen_h369@live.co.uk>
2020-06-17 18:29:30 +0100

    #9 exercise b complete

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -85,8 +85,12 @@ users = {
 
 # 8. Change Erik's hometown to Edinburgh
 
-users["Erik"][:home_town] = "Edinburgh"
-p users["Erik"][:home_town]
+# users["Erik"][:home_town] = "Edinburgh"
+# p users["Erik"][:home_town]
 
 # 9. Add a pet dog to Erik called "Fluffy"
+
+users["Erik"][:pets].push({name: "fluffy", species: "dog"})
+p users["Erik"][:pets]
+
 # 10. Add another person to the users hash
