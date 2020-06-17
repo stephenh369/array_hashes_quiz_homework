ab42433bc7a84cef41ef104ce8a9167be1bcc395
Stephen <stephen_h369@live.co.uk>
2020-06-17 16:15:02 +0100

    #4 exercise a complete

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -12,11 +12,16 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 
-stops.insert(3, "Polmont")
-p stops
+# stops.insert(3, "Polmont")
+# p stops
 
 #4. Print out the index position of "Linlithgow"
+
+p stops.index("Linlithgow")
+
 #5. Remove "Livingston" from the array using its name
+
+
 #6. Delete "Cumbernauld" from the array by index
 #7. Print the number of stops there are in the array?
 #8. Show as many ways as you can to return "Falkirk High" from the array?
